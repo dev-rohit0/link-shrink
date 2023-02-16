7df8670cd1d04d82d38fd0f66e5bae6da9ab6341
dev-rohit0 <rohit.codein@gmail.com>
2023-02-17 00:05:53 +0530

    somefix

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-dotenv.config();
-
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY ,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
-  projectId: process.env.REACT_APP_PROJECT_ID ,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET ,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID ,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyCDFFutlnCYU6Fw0HVQWHOJw7nWcE4f92E",
+  authDomain: "link-shrink-125b2.firebaseapp.com",
+  projectId: "link-shrink-125b2",
+  storageBucket: "link-shrink-125b2.appspot.com",
+  messagingSenderId: "307666355656",
+  appId: "1:307666355656:web:45d3470e5739b579916cfd"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
